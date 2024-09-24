e31b88065bab2c5837750bfe7137cab1d8565a3e
Julio-SC <jhver2001@gmail.com>
2024-09-24 12:12:00 -0700

    Ex 13 solved

--- ex13.js
+++ ex13.js
@@ -1,7 +1,16 @@
 // Exercise 13: Write a function named 'findById' that takes an array of objects and an id, and returns the object with the matching id, or null if not found.
 
 function findById(arr, id) {
-  // your code here
+
+let match = null
+
+  for(let x = 0; x < arr.length; x++) {
+    if(arr[x].id == id) {
+      match = arr[x];
+    }
+}
+return match
+// your code here
 }
 
-console.log(findById([{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }], 2)) // { id: 2, name: 'Jane' }
+console.log(findById([{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }], 3)) // { id: 2, name: 'Jane' }
