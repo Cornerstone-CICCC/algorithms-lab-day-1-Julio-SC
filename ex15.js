353a10f0541314e3b32d95d5f9b94541263a9934
Julio-SC <jhver2001@gmail.com>
2024-09-24 12:12:51 -0700

    Ex 15 trying to solve

--- ex15.js
+++ ex15.js
@@ -1,6 +1,16 @@
 // Exercise 15: Write a function named 'countWords' that takes a string of text and returns the number of words in the string.
 
 function countWords(text) {
+  
+  let total = 0
+
+  for(x = 0; x < text.length; x++) {
+    if(!text[x] === "") {
+      total = text[x]
+    }
+  }
+  
+  return total
   // your code here
 }
 
